9758e97744e4407ada0f3d2b18c8cad5ab7ab0ac
akupsmee <akupuomeikenna@gmail.com>
2025-03-06 16:46:19 +0100

    add github action and aws-cdk

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    });
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: hello,
+    });
   }
 }
